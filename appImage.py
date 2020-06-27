66a6f3f00606ecfef26340d6897a736a598d63c8
swapnilnagare21 <67508498+swapnilnagare21@users.noreply.github.com>
2020-06-27 23:04:01 +0530

    Add files via upload

---
+++ appImage.py
@@ -0,0 +1,63 @@
+#Import necessary packages
+from flask import Flask
+from flask_restful import Resource, reqparse, Api
+import base64
+import requests
+import cStringIO
+from io import BytesIO
+from PIL import Image
+
+#Instantiate a flask object 
+app = Flask(__name__)
+#Instantiate Api object
+api = Api(app)
+
+app.app_context().push()
+
+
+
+class ResizeImage(Resource):
+    def get(self):        
+        imgbbKey = '7560011cb554421936fd80696b850a18'
+
+        requiredWidth = 500;
+        requiredHeight = 750;
+
+
+        titleNames = ["The Dark Knight"]
+        imageIds = ["https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"]
+
+
+        imageUrl = imageIds[0]
+        img_data = requests.get(imageUrl).content
+
+        image = Image.open(BytesIO(img_data))
+        resizedImage = image.resize((requiredWidth,requiredHeight), Image.NEAREST)
+
+
+
+        buffered = cStringIO.StringIO()
+        resizedImage.save(buffered, format="JPEG")
+        #img_str = base64.b64encode(buffered.getvalue())
+
+
+        url = "https://api.imgbb.com/1/upload"
+
+        payload = {
+            "key": imgbbKey,
+            "image": base64.b64encode(buffered.getvalue()),
+            "expiration": 300,
+        }
+        res = requests.post(url, payload)
+
+        uploadedImageJson = res.json()
+
+        return uploadedImageJson
+
+
+#Adding the URIs to the api
+api.add_resource(ResizeImage, '/')
+
+if __name__=='__main__':        
+    #Run the applications
+    app.run()
